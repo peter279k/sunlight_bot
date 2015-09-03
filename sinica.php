7f638e33bfba91657339f581d38189597ee7ec0c
peter279k <peter279k@gmail.com>
2015-09-04 02:20:48 +0800

    Add detect_os to change encoding

--- sinica.php
+++ sinica.php
@@ -3,7 +3,11 @@
 	require 'libs/LIB_parse.php';
 	
 	//user input by command
-	echo iconv("utf-8", "big5", "請輸入斷字");
+	$detect_os = PHP_OS;
+	if(stristr($detect_os, "WIN"))
+		echo iconv("utf-8", "big5", "請輸入斷字: ");
+	else
+		echo "請輸入斷字: ";
 	$handle = fopen("php://stdin", "r");
 	$input_str = fgets($handle);
 	$input_str = trim($input_str);
@@ -33,6 +37,9 @@
 		$temp_str = return_between($response_link[$count], "<a>", "</a>", EXCL);
 		$temp_str = explode(">", $temp_str);
 		//$temp_link = get_attribute($temp_str[0], "HREF");
-		echo "\n" . iconv("utf-8", "big5", $temp_str[1]) . ": " . "http://sunlight.iis.sinica.edu.tw/" . get_attribute(str_replace($search_arr, '"', $response_link2[$count]), "HREF") . "\n";
+		if(stristr($detect_os, "WIN"))
+			echo "\n" . iconv("utf-8", "big5", $temp_str[1]) . ": " . "http://sunlight.iis.sinica.edu.tw/" . get_attribute(str_replace($search_arr, '"', $response_link2[$count]), "HREF") . "\n";
+		else
+			echo "\n" . "utf-8", "big5", $temp_str[1] . ": " . "http://sunlight.iis.sinica.edu.tw/" . get_attribute(str_replace($search_arr, '"', $response_link2[$count]), "HREF") . "\n";
 	}
 ?>
