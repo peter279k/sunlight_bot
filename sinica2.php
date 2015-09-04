66ed399ad81bb78a56a8e89cace09cb1a6f3f105
peter279k <peter279k@gmail.com>
2015-09-04 12:33:58 +0800

    Add read file version

---
+++ sinica2.php
@@ -0,0 +1,45 @@
+<?php
+	require 'libs/LIB_http.php';
+	require 'libs/LIB_parse.php';
+	
+	//user input by command
+	$detect_os = PHP_OS;
+	$handle = file_get_contents("input.txt");
+	$input_str = iconv("utf-8", "big5", $handle);
+	$input_str = trim($input_str);
+	$action = "http://sunlight.iis.sinica.edu.tw/cgi-bin/text.cgi";
+	$data_arr = array();
+	
+	$data_arr["query"] = $input_str;
+	$result = http($action, $ref = "", $method = "POST", $data_arr, EXCL_HEAD);
+	$result_str = $result["FILE"];
+	$result_arr = parse_array($result_str, "<META ", ">");
+	$res_url = (get_attribute($result_arr[1], "CONTENT"));
+	$res_url = explode("=", $res_url);
+	$res_url = trim($res_url[1], "'");
+	echo "\n" . "response_url: http://sunlight.iis.sinica.edu.tw/" . $res_url . "\n";
+	
+	//parse response url
+	$web_page = http_get("http://sunlight.iis.sinica.edu.tw/" . $res_url, $ref = "");
+	$web_page = trim($web_page["FILE"], "<br>");
+	
+	$response_link = parse_array($web_page, "<a ", "</a>");
+	$response_link2 = parse_array($web_page, "<a ", ">");
+	$search_arr = array("'", '"');
+	
+	$counter = count($response_link);
+	for($count=0;$count<$counter;$count++)
+	{
+		$temp_str = return_between($response_link[$count], "<a>", "</a>", EXCL);
+		$temp_str = explode(">", $temp_str);
+		$link = "http://sunlight.iis.sinica.edu.tw/" . get_attribute(str_replace($search_arr, '"', $response_link2[$count]), "HREF");
+		$web_page = http_get($link, $ref = "");
+		$web_page = $web_page["FILE"];
+		$plain_text = return_between($web_page, "<pre>", "</pre>", EXCL);
+		$plain_text = str_replace("-", "", trim($plain_text));
+		if(stristr($detect_os, "WIN"))
+			echo "\n" . iconv("utf-8", "big5", $temp_str[1]) . ": " . $plain_text . "\n";
+		else
+			echo "\n" . "utf-8", "big5", $temp_str[1] . ": " . $plain_text . "\n";
+	}
+?>
